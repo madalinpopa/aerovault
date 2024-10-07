324383d9bb2b4386bd97d207287195c3b834fd0d
Madalin Popa <contact@madalinpopa.com>
2024-10-07 11:08:02 +0300

    🛠️ Add path utilities and unit tests

---
+++ internal/utils/path.go
@@ -0,0 +1,75 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const (
+
+	// errOutputPathNotExist indicates that the specified output path does not exist.
+	errOutputPathNotExist = "output path does not exist"
+
+	// errOutputPathNotDir indicates that the specified output path is not a directory.
+	errOutputPathNotDir = "output path is not a directory"
+
+	// errGettingOutputPathInfo is used to signal that there was an error retrieving information about the output path.
+	errGettingOutputPathInfo = "error getting output path info: %v"
+)
+
+// isNotExist checks whether an error corresponds to a file or directory not existing.
+func isNotExist(err error) bool {
+	return os.IsNotExist(err)
+}
+
+// ValidateOutputPath checks if the given output directory path is valid, exists, and is a directory.
+// Returns the absolute path if valid, otherwise returns an error.
+func ValidateOutputPath(output string) (string, error) {
+	info, err := os.Stat(output)
+	if err != nil {
+		if isNotExist(err) {
+			return "", fmt.Errorf(errOutputPathNotExist)
+		}
+		return "", fmt.Errorf(errGettingOutputPathInfo, err)
+	}
+	if !info.IsDir() {
+		return "", fmt.Errorf(errOutputPathNotDir)
+	}
+	return filepath.Abs(output)
+}
+
+// GetCurrentDir retrieves and returns the current working directory. Returns an empty string and an error if any occurs.
+func GetCurrentDir() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("error getting current dir: %v", err)
+	}
+	return dir, nil
+}
+
+// GetResolvedOutputPath resolves the provided directory path; retrieves the current working directory if the path is empty.
+func GetResolvedOutputPath(path string) (string, error) {
+	if path == "" {
+		return handleEmptyPath()
+	}
+	return handleProvidedPath(path)
+}
+
+// handleEmptyPath retrieves and returns the current working directory. If it fails, it returns an empty string and an error.
+func handleEmptyPath() (string, error) {
+	currentDir, err := GetCurrentDir()
+	if err != nil {
+		return "", err
+	}
+	return currentDir, nil
+}
+
+// handleProvidedPath validates the provided directory path and returns its absolute path if valid, otherwise returns an error.
+func handleProvidedPath(path string) (string, error) {
+	validatedPath, err := ValidateOutputPath(path)
+	if err != nil {
+		return "", err
+	}
+	return validatedPath, nil
+}
---
+++ internal/utils/path_test.go
@@ -0,0 +1,134 @@
+package utils_test
+
+import (
+	"github.com/madalinpopa/aerovault/internal/utils"
+	"os"
+	"testing"
+)
+
+const (
+	tempDirPattern = "container-volume-backup-test-*"
+)
+
+func createTempFile(t *testing.T) *os.File {
+	t.Helper()
+	f, err := os.CreateTemp(os.TempDir(), tempDirPattern)
+	if err != nil {
+		t.Fatalf("failed to create temp file: %s", err)
+	}
+	return f
+}
+
+func removeTempFile(t *testing.T, fileName string) {
+	t.Helper()
+	err := os.Remove(fileName)
+	if err != nil {
+		t.Fatalf("failed to remove temp file: %s", err)
+	}
+}
+
+func createTempD(t *testing.T) string {
+	t.Helper()
+	tempD, err := os.MkdirTemp(os.TempDir(), tempDirPattern)
+	if err != nil {
+		t.Fatalf("failed to create temp dir: %s", err)
+	}
+	return tempD
+}
+
+func removeTempD(t *testing.T, dirName string) {
+	t.Helper()
+	err := os.RemoveAll(dirName)
+	if err != nil {
+		t.Fatalf("failed to remove temp dir: %s", err)
+	}
+}
+
+func TestValidateOutputPath_pathNotExists(t *testing.T) {
+
+	_, err := utils.ValidateOutputPath("not-exists")
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+
+	if err != nil && err.Error() != "output path does not exist" {
+		t.Errorf("expected error message: 'output path not exists', got: %s", err.Error())
+	}
+}
+
+func TestValidateOutputPath_pathExists(t *testing.T) {
+
+	// create a temporary directory
+	tempD := createTempD(t)
+	defer removeTempD(t, tempD)
+
+	_, err := utils.ValidateOutputPath(tempD)
+	if err != nil {
+		t.Errorf("expected no error, got %s", err)
+	}
+
+}
+
+func TestValidateOutputPath_pathIsFile(t *testing.T) {
+	// create a temp file
+	f := createTempFile(t)
+	defer removeTempFile(t, f.Name())
+
+	_, err := utils.ValidateOutputPath(f.Name())
+	if err == nil {
+		t.Errorf("expected error, got nil: %s", err)
+	}
+
+	if err.Error() != "output path is not a directory" {
+		t.Errorf("expected error message: 'output path is a file', got: %s", err.Error())
+	}
+}
+
+func TestGetCurrenDir(t *testing.T) {
+	dir, err := utils.GetCurrentDir()
+	if err != nil {
+		t.Errorf("expected no error, got %s", err)
+	}
+	if dir == "" {
+		t.Errorf("expected dir, got empty string")
+	}
+
+	osDir, osErr := os.Getwd()
+	if osErr != nil {
+		t.Fatalf("os.Getwd failed: %v", osErr)
+	}
+
+	if dir != osDir {
+		t.Errorf("GetCurrentDir returned %s, but os.Getwd returned %s", dir, osDir)
+	}
+}
+
+func TestGetResolvedOutputPath_validPath(t *testing.T) {
+	// create a temporary directory
+	tempD := createTempD(t)
+	defer removeTempD(t, tempD)
+
+	p, err := utils.GetResolvedOutputPath(tempD)
+	if err != nil {
+		t.Errorf("expected no error, got %s", err)
+	}
+
+	if p != tempD {
+		t.Errorf("expected %s, got %s", tempD, p)
+	}
+}
+
+func TestGetResolvedOutputPath_emptyPath(t *testing.T) {
+	osDir, osErr := os.Getwd()
+	if osErr != nil {
+		t.Fatalf("os.Getwd failed: %v", osErr)
+	}
+
+	p, err := utils.GetResolvedOutputPath("")
+	if err != nil {
+		t.Errorf("expected no error, got %s", err)
+	}
+	if p != osDir {
+		t.Errorf("expected string %s, got %s", osDir, p)
+	}
+}
