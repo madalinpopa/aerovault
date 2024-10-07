36c3152e64f52430599cce1bb1b7c61ed6b6a9fa
Madalin Popa <contact@madalinpopa.com>
2024-10-07 11:07:47 +0300

    🎉 Add versioning to config with GetVersion function

---
+++ config.go
@@ -0,0 +1,9 @@
+package aerovault
+
+// Version represents the current version of the application.
+const version = "0.1.0"
+
+// GetVersion returns the current version of the application.
+func GetVersion() string {
+	return version
+}
